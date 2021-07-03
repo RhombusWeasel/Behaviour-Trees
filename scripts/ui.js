9d82034e5134e73bbd70b9981a408867d355ed35
Pete Hunter <pete.hunter@piksel.com>
2021-07-03 23:22:46 +0100

    Testing token HUD.

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,24 +1,18 @@
 let ui_log = new bt_logger('/scripts/ui.js');
 
-Hooks.on("getSceneControlButtons", (controls, b, c) => {
+Hooks.on("'renderTokenHUD'", (hud, html, token) => {
 
     if (game.user.isGM) {
-        let ai_tool = {
-        name: "setTokenAI",
-        title: "AI Settings",
-        icon: "fas fa-brain",
-        toggle: false,
-        active: true,
-        onClick: (toggle) => {
-            let selected = bt.user.get_selected();
-            if (selected == false) {
-                ui_log.error('No Token Selected.');
-            } else {
-                ui_log.debug('Selected Token:', selected);
+        let ai_tool = `
+            <a><i class="fas fa-brain"></i></a>
+        `
+        html.find('div.right')
+            .append(ai_tool)
+            .click((event) => {
+                ui_log.debug('Behaviour Tree Button clicked.', hud, html, token);
             }
-        },
-        };
-        controls.find((c) => c.name == "token").tools.push(ai_tool);
+        );
+        
     }
 
 });
