811d2aa1cd5e1249fc76d954d7e8e31bb54695a3
Pete Hunter <pete.hunter@piksel.com>
2021-07-03 23:32:46 +0100

    Testing token HUD.

--- scripts/ui.js
+++ scripts/ui.js
@@ -3,6 +3,7 @@ let ui_log = new bt_logger('/scripts/ui.js');
 Hooks.on("'renderTokenHUD'", (hud, html, token) => {
 
     if (game.user.isGM) {
+        ui_log.debug('Behaviour Tree HUD.', hud, html, token);
         let ai_tool = `
             <div>
                 <a><i class="fas fa-brain"></i></a>
@@ -11,7 +12,7 @@ Hooks.on("'renderTokenHUD'", (hud, html, token) => {
         html.find('div.right')
             .append(ai_tool)
             .click((event) => {
-                ui_log.debug('Behaviour Tree Button clicked.', hud, html, token);
+                
             }
         );
         
