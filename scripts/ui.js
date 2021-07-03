48ef22515ebae0cbefccb620b1f7d504c773af91
Pete Hunter <pete.hunter@piksel.com>
2021-07-03 23:37:35 +0100

    Testing token HUD.

--- scripts/ui.js
+++ scripts/ui.js
@@ -4,7 +4,7 @@ Hooks.on("'renderTokenHUD'", (hud, html, token) => {
 
     if (game.user.isGM) {
         ui_log.debug('Behaviour Tree HUD.', hud, html, token);
-        let ai_tool = await renderTemplate('/modules/behaviour_trees/templates/hud.html', {})
+        let ai_tool = renderTemplate('/modules/behaviour_trees/templates/hud.html', {});
         html.find('div.right')
             .append(ai_tool)
             .click((event) => {
