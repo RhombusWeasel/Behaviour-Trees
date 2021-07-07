eba9481ea3796a7fb7b27af19b8be7f3e97821aa
Pete Hunter <pete.hunter@piksel.com>
2021-07-07 19:24:08 +0100

    CSS styling.

--- scripts/module.js
+++ scripts/module.js
@@ -37,7 +37,7 @@ Hooks.once('ready', async function() {
     bt.journal = game.journal.getName('Behaviour Trees Node Data');
     mod_log.debug('Node Data:', bt.journal);
     //if (bt.journal == undefined) {
-        bt.journal = new Journal({
+        bt.journal = new JournalEntryData({
             name: 'Behaviour Trees Node Data',
             content: '{}'
         });
