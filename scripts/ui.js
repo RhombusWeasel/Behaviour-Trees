3bc132fdc6cb6998325547a1cbbae00f88f52986
Pete Hunter <pete.hunter@piksel.com>
2021-07-10 11:22:49 +0100

    Button Fix.

--- scripts/ui.js
+++ scripts/ui.js
@@ -13,12 +13,14 @@ Hooks.on('ready', () => {
 Hooks.on('renderTokenHUD', async (hud, html, token) => {
     ui_log.debug('Behaviour Tree HUD.', hud, html, token);
     if (game.user.isGM) {
-        let ai_tool = await renderTemplate('/modules/behaviour_trees/templates/hud.html', {});
-        ai_tool.click = (event) => {
-            ui_log.debug(event);
-            new ai_builder(token).render(true);
-        };
-        html.find('div.right').append(ai_tool);
+        const ai_tool = await renderTemplate('/modules/behaviour_trees/templates/hud.html', {});
+        html.find('div.right')
+            .append(ai_tool)
+            .click((event) => {
+                ui_log.debug(event);
+                new ai_builder(token).render(true);
+            }
+        );
     }
 
 });
