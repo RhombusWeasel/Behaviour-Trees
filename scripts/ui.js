3904620b91befb9bc1a3a649c5b263153296af08
Pete Hunter <pete.hunter@piksel.com>
2021-07-10 12:12:59 +0100

    Button Fix.

--- scripts/ui.js
+++ scripts/ui.js
@@ -14,10 +14,9 @@ Hooks.on('renderTokenHUD', async (hud, html, token) => {
     ui_log.debug('Behaviour Tree HUD.', hud, html, token);
     if (game.user.isGM) {
         const ai_tool = await renderTemplate('/modules/behaviour_trees/templates/hud.html', {});
-        html.find('div.right')
-            .append(ai_tool)
-            .click((event) => {
-                ui_log.debug(event);
+        html.find('div.right').append(ai_tool);
+        html.getElementById('bt-ai-selector').click((event) => {
+                ui_log.debug('HUD Click Event.', event);
                 let element = event.currentTarget;
                 let action = element.closest(".control-icon").dataset.action;
                 if (action == "bt-ai-selector") {
